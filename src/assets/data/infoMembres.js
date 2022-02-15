1e264b278cd8a0d54a18b603295ca1661f85bbeb
Dale Limoges <limogesdale@outlook.com>
2022-02-15 11:40:34 -0500

    premier commit

---
+++ src/assets/data/infoMembres.js
@@ -0,0 +1,55 @@
+let infoMembres = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 1,
+        "ICONE_SRC":"./assets/images/00-09.png",
+        "IMG_SRC":"./assets/images/membre-1.jpg",
+        "IMG_ALT":"alt-01",
+        "NOM": "Martin Gingra",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":2002,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "martin.gingra@physiogingra.com",
+        "SITE_WEB": "physiogingra.com",
+        "ADRESSE": "4710-4500 Boul. Saint-Laurent",
+        "VILLE": "Montréal",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "H2T1R3",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.58785894418295, 45.5188020957226]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 2,
+        "ICONE_SRC":"./assets/images/90-99.png",
+        "IMG_SRC":"./assets/images/membre-2.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Benoit Barbeau",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1998,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "Benoit.Barbeau@physiobarbeau.com",
+        "SITE_WEB": "physiobarbeau.com",
+        "ADRESSE": "4728-4500 Rue Saint-Urbain",
+        "VILLE": "Montréal",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "H2T2V8",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.58884766292596, 45.52008779021251]
+      }
+    }
+  ]
+};
