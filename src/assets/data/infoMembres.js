b4455d92599bb000bd90de0ee1172541cf17764c
Dale Limoges <limogesdale@outlook.com>
2022-02-19 10:10:21 -0500

    Essaie pour filtre ou query geoJSON

--- src/assets/data/infoMembres.js
+++ src/assets/data/infoMembres.js
@@ -5,6 +5,8 @@ let infoMembres = {
       "type": "Feature",
       "properties": {
         "ID": 1,
+        "tous":true,
+        "0009":true,
         "ICONE_SRC":"./assets/images/00-09.png",
         "IMG_SRC":"./assets/images/membre-1.jpg",
         "IMG_ALT":"alt-01",
@@ -30,6 +32,8 @@ let infoMembres = {
       "type": "Feature",
       "properties": {
         "ID": 2,
+        "tous":true,
+        "9099":true,
         "ICONE_SRC":"./assets/images/90-99.png",
         "IMG_SRC":"./assets/images/membre-2.jpg",
         "IMG_ALT":"alt-02",
@@ -38,8 +42,8 @@ let infoMembres = {
         "MEMBRE_DEPUIS":1998,
         "TELEPHONE":"438-933-7785",
         "TELEPHONE_REF":"tel:4389337785",
-        "COURRIEL": "Benoit.Barbeau@physiobarbeau.com",
-        "SITE_WEB": "physiobarbeau.com",
+        "COURRIEL": "Benoit.Barbeau@physio.com",
+        "SITE_WEB": "physio.com",
         "ADRESSE": "4728-4500 Rue Saint-Urbain",
         "VILLE": "Montréal",
         "PROVINCE": "Québec",
@@ -49,6 +53,310 @@ let infoMembres = {
       "geometry": {
         "type": "Point",
         "coordinates": [-73.58884766292596, 45.52008779021251]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 3,
+        "tous":true,
+        "9099":true,
+        "ICONE_SRC":"./assets/images/90-99.png",
+        "IMG_SRC":"./assets/images/membre-3.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Annabelle Leclerc",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1996,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "Annabelle.Leclerc@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "1717 Rue George Minot",
+        "VILLE": "Laval",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "H7M 6C8",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.70274260185124, 45.60956297455351]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 4,
+        "tous":true,
+        "avant1990":true,
+        "ICONE_SRC":"./assets/images/avant1990.png",
+        "IMG_SRC":"./assets/images/membre-4.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Manon Marte",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1982,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "manon.marte@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "1739 Rue Louis-Durocher",
+        "VILLE": "Laval",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "H7M 3P5",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.70602037269461, 45.60923635226072]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 5,
+        "ICONE_SRC":"./assets/images/00-09.png",
+        "IMG_SRC":"./assets/images/membre-5.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Jean Michaud",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":2005,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "jean.michaud@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "1690 Rue du Mont-Carmel",
+        "VILLE": "Laval",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "H7E 4Y3",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-73.68247168966336, 45.59516078644172]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 6,
+        "ICONE_SRC":"./assets/images/10-19.png",
+        "IMG_SRC":"./assets/images/membre-6.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Micheline Michaud",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":2012,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "micheline.michaud@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "2005 Ch Ste-Foy",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1S 2P6",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.26368318134278, 46.79095993176529]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 7,
+        "ICONE_SRC":"./assets/images/avant1990.png",
+        "IMG_SRC":"./assets/images/membre-7.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Robert Ménard",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1975,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "robert.ménard@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "2000 Rue Richer",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1V 1P1",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.2660927903729, 46.79277347043084]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 8,
+        "ICONE_SRC":"./assets/images/avant1990.png",
+        "IMG_SRC":"./assets/images/membre-8.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Marjorie Benoit",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1987,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "marjorie.benoit@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "1607 Bd de l'Entente",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1S 2V3",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.26168801371237, 46.793115790916694]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 9,
+        "ICONE_SRC":"./assets/images/avant1990.png",
+        "IMG_SRC":"./assets/images/membre-9.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Mohammed Isht",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1967,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "mohammed.isht@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "330 Rue de Carillon",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1K 5B6",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.23694867779582, 46.811060027007805]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 10,
+        "ICONE_SRC":"./assets/images/00-09.png",
+        "IMG_SRC":"./assets/images/membre-10.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Michael Gariépi",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":2006,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "michael.gariépi@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "360 Rue Saint-Vallier O",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1K 1K6",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.2386761647718, 46.81221962840487]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 11,
+        "ICONE_SRC":"./assets/images/90-99.png",
+        "IMG_SRC":"./assets/images/membre-11.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Jocelyne Letendre",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1992,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "jocelyne.letendre@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "7680 Chem. Samuel",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1H 2Z7",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.26710479972274, 46.86051756092676]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 12,
+        "ICONE_SRC":"./assets/images/10-19.png",
+        "IMG_SRC":"./assets/images/membre-12.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Richard Duchesne",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":2011,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "robert.duchesne@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "7960 Le Trait Carr East",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": " G1H 2Z2",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.2685537678456, 46.861527021882665]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 13,
+        "ICONE_SRC":"./assets/images/10-19.png",
+        "IMG_SRC":"./assets/images/membre-13.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Caroline Néron",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":2016,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "caroline.neron@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "772 Bd Louis-XIV",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": " G1H 1A2",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.26908354283236, 46.86178089433012]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": 14,
+        "ICONE_SRC":"./assets/images/avant1990.png",
+        "IMG_SRC":"./assets/images/membre-14.jpg",
+        "IMG_ALT":"alt-02",
+        "NOM": "Benoit Barbeau",
+        "TITRE":"Physiothérapeute",
+        "MEMBRE_DEPUIS":1957,
+        "TELEPHONE":"438-933-7785",
+        "TELEPHONE_REF":"tel:4389337785",
+        "COURRIEL": "Benoit.Barbeau@physio.com",
+        "SITE_WEB": "physio.com",
+        "ADRESSE": "1505 Bd de l'Entente",
+        "VILLE": "Québec",
+        "PROVINCE": "Québec",
+        "CODE_POSTAL": "G1S 2V2",
+        "SOMMAIRE": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dignissimos incidunt error tenetur rerum sed in deserunt illum qui voluptatum. Voluptatibus, qui hic? Amet nam, consectetur accusamus dolor ipsa deserunt. Aliquid et eius officiis nam sapiente cumque nobis expedita doloribus non cupiditate eveniet, facere saepe animi amet dolore exercitationem odit ex tempora? Eveniet consequuntur dolor error id quis cum nesciunt!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-71.25975124601798, 46.794175346441115]
       }
     }
   ]
